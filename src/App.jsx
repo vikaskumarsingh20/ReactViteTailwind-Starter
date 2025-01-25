f533d6363701cbf6a17aeb3e5ad9ea21daaa249d
VikasKumarSingh <vikaskumarsingh0931@gmail.com>
2025-01-26 02:27:54 +0530

    React vite ,Tailwind Stater

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import  './index.css'
+
+
+  function App() {
+    return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center mx-auto">
+        <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      </div>
+    );
+  }
+
+  
+  export default App;
